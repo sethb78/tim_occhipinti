9fe58e43fac9d8e86928365a5f8cbcd5821bfd94
Seth B. Altshuler <sethb78@gmail.com>
2012-12-28 11:47:40 -0500

    delete initializers

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "966CCHa1WCXufbwhXbr8g", "rXgr70ofImEEiYubx5bYRImaddHMLKZycooXSwgOg"
-end
--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = '966CCHa1WCXufbwhXbr8g'
-  config.consumer_secret = 'rXgr70ofImEEiYubx5bYRImaddHMLKZycooXSwgOg'
-  config.oauth_token = '105156121-SAiKCh1mtF2kwQLAU3An5sAGmbjq67HAnmjmVtVs'
-  config.oauth_token_secret = 'D2eUAqGepLVGz1xQZvkyTf4gS3Mz243VxWdIqqivmHY'
-end
