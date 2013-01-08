e805f7fed3e8bdc500e35a1fdb5414dfd8ca956b
Seth B. Altshuler <sethb78@gmail.com>
2013-01-08 18:24:45 -0500

    update tabs

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ TimOcchipinti::Application.routes.draw do
   root to:'pages#home'
 
   match '/about_tim',  	            to: 'pages#about_tim'
-  match '/media',                   to: 'pages#media'
   match '/contribute',              to: 'pages#contribute'
-  match '/photos',                  to: 'pages#photo'
   match '/title',                   to: 'pages#title'
   match '/contact',		              to: 'contact_us/contacts#new'
   match '/admin',                   to: 'pages#admin'
