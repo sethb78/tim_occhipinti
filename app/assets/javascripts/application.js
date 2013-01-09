53a01759e73174085a3e8a9e2af790418d98754f
Seth B. Altshuler <sethb78@gmail.com>
2013-01-08 22:28:35 -0500

    js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require fantastic.galleria.js
 //= require bootstrap-transition
 //= require bootstrap-alert
 //= require bootstrap-modal
