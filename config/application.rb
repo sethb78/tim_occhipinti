703747d263a5d1a687bf7de0e01a6d107649ef47
Seth B. Altshuler <sethb78@gmail.com>
2013-01-18 18:01:32 -0500

    update app"

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
 
 module TimOcchipinti
   class Application < Rails::Application
