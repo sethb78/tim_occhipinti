b49cb17ccfc8a5ecda06f8e9dfd881874b08cc9f
Seth B. Altshuler <sethb78@gmail.com>
2013-01-30 09:47:03 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,11 @@ TimOcchipinti::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  AWS_ACCESS_KEY_ID = ENV['AMAZON_KEY_ID']
+  AWS_SECRET_ACCESS_KEY = ENV['AMAZON_SECRET_KEY']
+  AWS_S3_BUCKET = ENV['AMAZON_BUCKET_NAME']
+
+
 
 
   config.action_mailer.smtp_settings = {
