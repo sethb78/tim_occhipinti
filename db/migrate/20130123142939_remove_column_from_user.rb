cc9459801f8c0e9ed591153a8041d1d48cd2e67e
Seth B. Altshuler <sethb78@gmail.com>
2013-01-29 20:54:01 -0500

    update

--- db/migrate/20130123142939_remove_column_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :remote_image_url
-  end
-
-  def down
-    add_column :users, :remote_image_url, :string
-  end
-end
