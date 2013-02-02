8446580f2c703c2b2822fd6c267a772eb1ae7e31
Seth B. Altshuler <sethb78@gmail.com>
2013-02-01 23:17:48 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,14 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
+    if Rails.env == "production"
+        config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-38171296-1")
+    end
 
 
 module TimOcchipinti
   class Application < Rails::Application
-    if Rails.env == "production"
-        config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-38171296-1")
-    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
