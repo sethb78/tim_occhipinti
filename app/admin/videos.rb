c60001df5dda4129fec5ddd68337af3b2e09a1f5
Seth B. Altshuler <sethb78@gmail.com>
2013-02-03 16:55:11 -0500

    update adminm

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,16 +1,3 @@
 ActiveAdmin.register Video do
-  index do
-  	column :title
-  	column :link
-  	default_actions
-  end
-  form do |f|
-  	f.inputs do
-  		f.input :title
-  		f.input :description, label: "Description (optional)"
-  		f.input :link
-  		f.input :video_date
-  	end
- 	f.buttons
- end
+  
 end
