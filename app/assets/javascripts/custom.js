06509ce07614845131d568f99c3872a4eee85cfd
Seth B. Altshuler <sethb78@gmail.com>
2013-02-04 15:04:17 -0500

    update blog date

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,7 @@
-// $(function(){
-// $('.news-articles:odd').css('background-color', '#f0f0eb');
+ $(function(){
+// $('.news-article-link').click(function(){
+// 	$('.news-article-link').addClass('active');
+
 // });
+});
 
