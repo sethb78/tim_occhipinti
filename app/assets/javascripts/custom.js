c6471ac46f5b083ec18636ac03a131b15f1b3e81
Seth B. Altshuler <sethb78@gmail.com>
2013-02-06 13:38:25 -0500

    updated jquery carousel

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,19 @@
- $(function(){
- $('.news-article-link').click(function(){
- 	$('.news-link').addClass('active');
-
- });
+$(document).ready(function(){
+	rotatePics(1);
 });
+	function rotatePics(currentPhoto){
+		var numberOfPhotos = $('#photo-carousel-wheel img').length;
+		currentPhoto = currentPhoto % numberOfPhotos;
+
+		$('#photo-carousel-wheel img').eq(currentPhoto).fadeOut(function(){
+			$('#photo-carousel-wheel img').each(function(i){
+				$(this).css(
+					'zIndex', ((numberOfPhotos - i) + currentPhoto) %
+				numberOfPhotos
+				);
+			});
+			$(this).show();
+			setTimeout(function(){rotatePics(++currentPhoto);}, 3000);
+		});
+	}
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
-before_filter :signed_in_user, only: [:admin]
     def home
         @blog=Blog.last
         @latest_news=NewsArticle.find(:all, :limit => 5, :order => "date DESC")
       	@event=UpcomingEvent.find(:all, :conditions => ["event_start between ? and ?", (DateTime.now - 1.day), (DateTime.now + 2.months)], :limit => 5)
         @first_carousel_picture=CarouselPicture.order("RANDOM()").first
-        @carousel_pictures = CarouselPicture.find(:all, :conditions => ["id != #{@first_carousel_picture.id}"])
+        @carousel_pictures = CarouselPicture.all
         @video = Video.last
     end
 
     def media
     	@archive_news = NewsArticle.all(:order => "date DESC")
         @blogs = Blog.find(:all, :order => "created_at DESC")
+        @link=request.fullpath
     end
 end
