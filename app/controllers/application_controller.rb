b96c37f595724f8b8cdee7950cd8df62e72ed3da
Seth B. Altshuler <sethb78@gmail.com>
2013-02-16 00:17:10 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   include ContributionsHelper
 
     protect_from_forgery
+
+def after_sign_out_path_for(resource_or_scope)
+root_path
+end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,8 @@
 class BlogsController < ApplicationController
 	 def index
     	@archive_news = NewsArticle.all(:order => "date DESC")
-        @blogs = Blog.find(:all, :order => "created_at DESC")  	end
+        @blogs = Blog.find(:all, :order => "created_at DESC")  
+    end
 
 	def show
 		@full_blog=Blog.find(params[:id])
