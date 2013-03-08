b20369bcdead2f336ca8b9f8f0c9e791e6810d36
Seth B. Altshuler <sethb78@gmail.com>
2013-03-08 15:31:03 -0500

    update

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,24 +4,26 @@ $(document).ready(function(){
   var $parent=$('a[href="' + this.location.pathname + '"]').parent()
 
 if (!document.cookie.match(/(?:^|; *)alert_shown=1/)) {
-    $("#overlay_form").fadeIn(1000);
-    positionPopup();
-    $("#close").click(function(){
-    $("#overlay_form").fadeOut(500);
-    });
 
-function positionPopup(){
-  if(!$("#overlay_form").is(':visible')){
-  return;
-  }
+$("#overlay_form").fadeIn(1000);
 
-}
- $(window).bind('resize',positionPopup);
+ 
+//close popup
+$("#close").click(function(){
+$("#overlay_form").fadeOut(500);
+});
 
 
     document.cookie = "alert_shown=1;max-age=" + 60 * 60 * 24 * 365;
  }
 });
+ 
+
+ 
+//maintain the popup at center of the page when browser resized
+$(window).bind('resize',positionPopup);
+
+
 function slideShow() {
   var current = $('#photo-carousel-container .show');
   var next = current.next().length ? current.next() : current.parent().children(':first');
