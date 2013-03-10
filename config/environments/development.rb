a39c01b0f15aa8db64e0c5e13dcc8dee3a1e38f9
Seth B. Altshuler <sethb78@gmail.com>
2013-03-10 10:26:52 -0400

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ TimOcchipinti::Application.configure do
 
 
 
-config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
 
 
--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -4,8 +4,8 @@ ContactUs.setup do |config|
   # ==> Mailer Configuration
 
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
-  # Example:
-  config.mailer_from = "contact@please-change-me.com"
+  # # Example:
+  # config.mailer_from = "contact@please-change-me.com"
 
 
   # Configure the e-mail address which should receive the contact form email notifications.
