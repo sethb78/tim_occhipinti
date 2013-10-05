91b6d12b6651b030b49976fdcc1edc2abc75f130
Seth B. Altshuler <sethb78@gmail.com>
2013-10-04 22:31:25 -0400

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   include ContributionsHelper
+  before_filter :set_layout_variables
 
     protect_from_forgery
 
@@ -8,3 +9,8 @@ def after_sign_out_path_for(resource_or_scope)
 root_path
 end
 end
+
+def set_layout_variables
+        @first_carousel_picture=CarouselPicture.order("RANDOM()").first
+        @carousel_pictures = CarouselPicture.find(:all, :conditions => ["id != #{@first_carousel_picture.id}"])  
+      end
