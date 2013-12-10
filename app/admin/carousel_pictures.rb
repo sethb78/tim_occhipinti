44a74f0f777379f6983ce455b2ab3739d6f1a9ab
Seth B. Altshuler <sethb78@gmail.com>
2013-12-10 15:24:14 -0500

    updated active_admin/carousel_picture

--- app/admin/carousel_pictures.rb
+++ app/admin/carousel_pictures.rb
@@ -1,10 +1,16 @@
 ActiveAdmin.register CarouselPicture do
-
- 	form do |f|
- 		f.inputs do
-	 		f.input :image
- 			f.input :remote_image_url, :label => "or Image URL"
- 		end
- 		f.buttons
- 	end
+         index do
+                 column :id
+                 column :name
+                 column :image
+                 column :remote_image_url
+                 default_actions
+         end
+         form do |f|
+                 f.inputs do
+                         f.input :image
+                         f.input :remote_image_url, :label => "or Image URL"
+                 end
+                 f.buttons
+         end
 end
