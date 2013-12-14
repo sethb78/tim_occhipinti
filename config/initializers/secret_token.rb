059b9b978b5eb12d6af3d344cd62c974fad92527
Seth B. Altshuler <sethb78@gmail.com>
2013-12-14 12:02:23 -0500

    env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
+
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TimOcchipinti::Application.config.secret_token = '4eeacb5676fbe00d33198a290bb1c156cd9f868040a6afa4ffdd0d2e9e2878f64a59b1d773f9ab8a570b65e204bd3db45e457bccf0d62c5a614b3f4e006b0172'
+TimOcchipinti::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
